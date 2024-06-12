58945fde6509a403f645b809bf9c0a89e44d86ef
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2024-06-11 18:18:27 -0600

    feat(): register design

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,7 +1,64 @@
-import React from 'react'
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+import { Container, Content, Form, FormContainer, ImageContainer } from "@/styles/Register.style";
+import Image from "next/image";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function Register() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isDirty },
+  } = useForm({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
   return (
-    <div>Register</div>
-  )
+    <Container>
+      <Content>
+        <FormContainer>
+          <div>
+            <h1>Bienvenido!</h1>
+            <span>
+            Regístrate para acceder a consejos exclusivos y todo lo que necesitas para una piel radiante.
+            </span>
+          </div>
+          <Form>
+            <Input fullWidth control={control} name="username" label="Username" />
+            <Input fullWidth control={control} name="email" label="Email" />
+            <Input
+              fullWidth
+              control={control}
+              name="password"
+              label="Password"
+            />
+            <Button text="Regístrarse" />
+          </Form>
+          <div>
+            <span>
+              ¿Ya tienes cuenta?{" "}
+              <a href="/login" className="register">
+                Inicia sesión
+              </a>
+            </span>
+          </div>
+        </FormContainer>
+        <ImageContainer>
+          <Image
+            src="/img/register.jpg"
+            fill={true}
+            style={{
+              borderRadius: "50px",
+              padding: "24px",
+              objectFit: "cover",
+            }}
+          />
+        </ImageContainer>
+      </Content>
+    </Container>
+  );
 }
---
+++ src/styles/Register.style.js
@@ -0,0 +1,57 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: var(--primary-100);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 800px;
+  height: 500px;
+  background-color: var(--neutral);
+  border-radius: 25px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
+
+export const FormContainer = styled.div`
+  padding: 32px 40px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  h1 {
+    font-family: "Poppins", sans-serif;
+    font-weight: 600;
+    font-size: 22px;
+    margin: 0;
+  }
+  span {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    font-size: 12px;
+    color: var(--tertiary);
+  }
+  .register {
+    color: var(--primary-500);
+    margin-left: 5px;
+    font-weight: 600;
+    cursor: pointer;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const ImageContainer = styled.div`
+    padding: 24px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
