c37e7a8fe9ec77e53051d56e17e00d7f205a05b8
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2024-06-14 12:52:24 -0600

    feat(): get by user's post connected

--- src/pages/crud.js
+++ src/pages/crud.js
@@ -1,7 +1,70 @@
-import React from 'react'
+import GetAllBlogUseCase from "@/application/usecases/blogUseCase/GetAllBlogUseCase";
+import Card from "@/components/Card";
+import BlogRepo from "@/infraestructure/implementation/httpRequest/axios/BlogRepo";
+import {
+  ButtonContainer,
+  Container,
+  PostContainer,
+  Section,
+  Subtitle,
+  Title,
+} from "@/styles/Crud.style";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function Crud() {
+  const user = useSelector((state) => state.user.username);
+  const [blogs, setBlogs] = useState([]);
+
+  const blogRepo = new BlogRepo();
+  const getAllBlogUseCase = new GetAllBlogUseCase(blogRepo);
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.toLocaleDateString("es-ES", { month: "long" });
+    const year = date.getFullYear();
+    return `${day} ${month}, ${year}`;
+  };
+
+  const fetchBlogs = async () => {
+    try {
+      const response = await getAllBlogUseCase.run();
+      const filteredBlogs = response.response.blogs.filter(
+        (blog) => blog.user === user
+      );
+      console.log(filteredBlogs);
+      setBlogs(filteredBlogs);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, [user]);
+
   return (
-    <div>Crud</div>
-  )
+    <Container>
+      <Title>Mis publicaciones</Title>
+      <ButtonContainer>
+        <button>Agregar</button>
+      </ButtonContainer>
+      <div>
+        <Subtitle>Publicaciones</Subtitle>
+        <Section>
+          {blogs.map((blog, index) => (
+            <Card
+              key={index}
+              image={blog.image.secureUrl}
+              alt={blog.title}
+              title={blog.title}
+              date={formatDate(blog.createdAt)}
+              user={blog.user}
+            />
+          ))}
+        </Section>
+      </div>
+    </Container>
+  );
 }
---
+++ src/styles/Crud.style.js
@@ -0,0 +1,36 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  margin: 16px 32px;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: end;
+`;
+
+export const Title = styled.h1`
+  font-family: "Poppins", sans-serif;
+  font-size: 32px;
+  font-weight: 700;
+  color: var(--secondary-700);
+`;
+
+export const Subtitle = styled.span`
+    font-family: "Poppins", sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    color: var(--tertiary-inverse);
+`;
+
+export const Section = styled.section`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  padding-left: 3%;
+  margin-top: 80px;
+  margin-bottom: 10%;
+  grid-gap: 80px;
+  @media(max-width: 1600px){
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
